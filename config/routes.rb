cd883eb4f814904a4a57ebdbf42eb69722680161
Emmanuel Addo-Yirenkyi <emmanuel.addo.yirenkyi@gmail.com>
2013-05-27 14:27:50 +0000

    Finished Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ClinicConsult::Application.routes.draw do
+   root to: 'pages#home'
+  match '/help',    to: 'pages#help',    via: 'get'
+  match '/about',   to: 'pages#about',   via: 'get'
+  match '/contact', to: 'pages#contact', via: 'get'  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
